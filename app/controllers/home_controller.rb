d11f4adb55f3b6d419ef368c025e657a2551d3bc
gzim921 <gzimiseni7@gmail.com>
2021-06-08 22:47:50 +0200

    added stock table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
   def index
-    if params[:ticker] == ""
+    StockQuote::Stock.new(api_key: 'pk_cd277a5a98b1430c9bd2ee3a586c6eb2')
+    if params[:ticker] == ''
       @nothing = 'Hey! You Forgot To Enter A Symbol'
     elsif params[:ticker]
       @stock = StockQuote::Stock.quote(params[:ticker])
     elsif !@stock
-      @error = "Hey! Thats Stock symbol Doesn't Exist. Please try Again!"
+      @error = "Hey! That Stock Symbol Doesn't Exist. Please Try Again."
     end
   end
 
