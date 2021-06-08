993c3cf44ef66d439b6424f41d6b7ada0a1cc023
gzim921 <gzimiseni7@gmail.com>
2021-06-08 15:25:35 +0200

    installed devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    if params[:ticker] == ""
+      @nothing = 'Hey! You Forgot To Enter A Symbol'
+    elsif params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
+    elsif !@stock
+      @error = "Hey! Thats Stock symbol Doesn't Exist. Please try Again!"
+    end
+  end
+
+  def about
   end
 end
